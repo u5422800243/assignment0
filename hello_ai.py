81914ff9716c1cef3d099934960f14a03c5b5403
u5422800243 <ccgg1234.jc@gmail.com>
2016-01-25 13:50:07 +0900

    commit

---
+++ hello_ai.py
@@ -0,0 +1,3 @@
+Python 3.4.4 (v3.4.4:737efcadf5a6, Dec 20 2015, 20:20:57) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("ID: 5422800243 Name: Yeonje Cho")
